4d986d7753a6a446b702ab54518619b9a3ba3f0d
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-11 13:27:29 -0400

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  #create
+  get 'tasks/new', to: 'restaurant#new'
+  post 'tasks', to: 'tasks#create'
+  #update
+  get 'tasks/:id/edit', to: 'restaurant#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  #delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
