135df6fc6f98bf91c09078bce59f2495fc95de09
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-12 13:00:54 -0400

    tasksfinal

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:task])
+    task = Task.new(task_params)
+    task.save
+    redirect_to task_path(task)
   end
 
   def edit
@@ -20,9 +22,19 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
