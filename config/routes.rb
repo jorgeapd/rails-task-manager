9ef0997c04c2438eba358961ae119bbe5c874058
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-11 22:49:30 -0400

    tasks3

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: :tasks
   get 'tasks/:id', to: 'tasks#show', as: :task
   #create
-  get 'tasks/new', to: 'restaurant#new'
+  get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
   #update
-  get 'tasks/:id/edit', to: 'restaurant#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit'
   patch 'tasks/:id', to: 'tasks#update'
   #delete
   delete 'tasks/:id', to: 'tasks#destroy'
