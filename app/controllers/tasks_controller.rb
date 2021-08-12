37e3f495b5ebb7a0b68904d4ad5b026aa091ba83
jorge pilcante <jorgeapd5@hotmail.com>
2021-08-11 23:30:25 -0400

    tasks4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,12 +8,15 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(params[:task])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
